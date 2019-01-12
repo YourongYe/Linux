33f1e91c9c497148f3e54d671d6b591d4b190ef8
Yourong Ye <34338026+YourongYe@users.noreply.github.com>
2019-01-12 10:30:10 +0800

    Update crontab.py

--- crontab.py
+++ crontab.py
@@ -14,17 +14,17 @@ root 身份登录到命令行
   
 # 如果执行遇到问题，系统会自动发邮件给用户；终端会显示‘You have new mail’
 # 邮件信息存在 /var/mail/YoYo 路径下
-# 在Finder里找到Macintosh HD 并 按组合键：cmd + shift + '>', 即可找到文件并删除
+# 在Finder里找到Macintosh HD 并 按组合键：cmd + shift + '>', 即可找到隐藏文件并删除
 
-SHELL=/bin/bash 
-PATH=/sbin:/bin:/usr/sbin:/usr/bin 
-MAILTO=root
-*/2 * * * * root python /Users/YoYo/Desktop/Intelligent_Invest/program/AutoHTML.py
-12 16 * * * root python /Users/YoYo/Desktop/Intelligent_Invest/program/AutoHTML.py
-16 16 * * * /anaconda3/bin/python3.exe /Users/YoYo/Desktop/Intelligent_Invest/program/AutoHTML.py
-# 编辑器改为nano
+
+PATH=/anaconda3/bin # 指定了python3的位置
+35 17 * * * python3 /Users/YoYo/Desktop/Intelligent_Invest/program/AutoHTML.py # 每天下午5点35执行
+*/2 * * * * python3 /Users/YoYo/Desktop/Intelligent_Invest/program/AutoHTML.py # 每隔两个分钟执行一次
+0 0 * * 2-6 python3 /Users/YoYo/Desktop/Intelligent_Invest/program/AutoHTML.py # 每周二到周六midnight执行
+
+
+# 编辑器改为nano或者vim
 export EDITOR=/usr/bin/nano
+export EDITOR=/usr/bin/vim
 
 
-PATH=/anaconda3/bin
-35 17 * * * python3 /Users/YoYo/Desktop/Intelligent_Invest/program/AutoHTML.py
