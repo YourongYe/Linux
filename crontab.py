67635d5194e729a5683a9718cb015b64669f6bc6
Yourong Ye <34338026+YourongYe@users.noreply.github.com>
2019-01-12 10:31:15 +0800

    Update crontab.py

--- crontab.py
+++ crontab.py
@@ -5,11 +5,9 @@ crontab -l
 crontab -r
 
 # 创建 crontab 文件的最简单方法
-root 身份登录到命令行
 输入 crontab -e
-按下 a 键进入到编辑模式
-输入 0 */1 * * * /home/work/start-service.sh
-同时按下 ctrl+c 退出编辑模式
+按下 a或者i 键进入到编辑模式
+按下 ctrl+c 退出编辑模式
 按下 shift+: 输入 wq 退出 crontab
   
 # 如果执行遇到问题，系统会自动发邮件给用户；终端会显示‘You have new mail’
